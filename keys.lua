afb1e59eeb0b324e3351ef6c0a4c82855c87206b
TukiminWijoyo <qhiyassyahla44@gmail.com>
2026-02-12 00:43:15 +0700

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -16,7 +16,7 @@ return {
     device = "YaeDG29tjSISKjExznRHiregx3Ynfc"
   },
   ["YaewufUbaRqxzGz"] = {
-    expire = "2009-09-09",
+    expire = "9999-09-09",
     device = "YaeQq67b5hWWcDjxB7kQ3jzsq5LGsb"
   }
 }
